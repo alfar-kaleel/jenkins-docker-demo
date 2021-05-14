8b88cc3a8fb58f1eb5b7916b2bd0d0b13452038b
alfar-kaleel <alfarkaleel@gmail.com>
2021-05-14 15:56:14 +0530

    second commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,6 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Hi, Jenkins is an awesome tool to integrate");
+        System.out.println("Hi all, Jenkins is an awesome tool to integrate");
     }
 }
